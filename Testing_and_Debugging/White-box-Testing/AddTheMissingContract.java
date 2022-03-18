7e04fef48d6e49018b6399d243cac23f248ac375
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-18 14:58:39 +0100

    Update AddTheMissingContract.java

--- Testing_and_Debugging/White-box-Testing/AddTheMissingContract.java
+++ Testing_and_Debugging/White-box-Testing/AddTheMissingContract.java
@@ -1,8 +1,8 @@
 public class ParenMatch {
 …
-public boolean parenMatch() {
-assert stack.isEmpty();
-for (int i=0; i<line.length(); i++) {
+  public boolean parenMatch() {
+  assert stack.isEmpty();
+  for (int i=0; i<line.length(); i++) {
 …
 }
 }
