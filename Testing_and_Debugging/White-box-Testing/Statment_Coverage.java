ff174b4469f6809a9ee7ee6734bc6b8fcd5da7b7
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-18 14:38:22 +0100

    CFG Statement Coverage
    public boolean parenMatch() {
    for (int i = 0; i< line.length(); i++ {
    char c = line.charAt(i);
    if(isLeftParen(c)) {
    stack.push(matchingRIghtParen(c));
    }
    
    
    Requires every branch edge in the CFG to be executed by at least one test 
    else if (isRightParen(c))) {
    stack.push(matchingRightParen(c));
    }
    else if (isRightParen(c)) {
    if (stack.isEmpty()) {
    return false;
    }
    if (stack.top().equals(C)) {
    stack.pop();
    } else{ return false; }
    }
    }
    return stack.isEmpty();
    }

---
+++ Testing_and_Debugging/White-box-Testing/Statment_Coverage.java
@@ -0,0 +1,16 @@
+public boolean parenMatch() {
+for (int i=0; i<line.length(); i++) { 
+char c = line.charAt(i); 
+if (isLeftParen(c)) { 
+stack.push(matchingRightParen(c));
+}
+else if (isRightParen(c)) {
+if (stack.isEmpty()) { 
+return false; 
+}
+if (stack.top().equals(c)) { 
+stack.pop(); 
+} else { return false; } 
+}
+} return stack.isEmpty(); 
+}
