b002a5b8a5d75cc56d3729269718e1c00ece3844
Esther Hoeun Yu <hoeuyu@student.ethz.ch>
2022-03-18 14:49:26 +0100

    Create Outlook.java

---
+++ Testing_and_Debugging/White-box-Testing/Outlook.java
@@ -0,0 +1,16 @@
+public boolean parenMatch() {
+    for (int i=0; i<line.length(); i++) { (1)
+      char c = line.charAt(i); (2)
+      if (isLeftParen(c)) { (3)
+        stack.push(matchingRightParen(c)); (4)
+}
+      if (isRightParen(c)) { (5)
+        if (stack.isEmpty()) { (6)
+          return false; (7)
+}
+      if (stack.top().equals(c)) { (8)
+        stack.pop(); (9)
+      } else { return false; } (10)
+}
+    } return stack.isEmpty(); (11)
+}
